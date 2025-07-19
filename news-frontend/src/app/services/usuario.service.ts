1ce16a8705cd175eead61e34a0cc00bd5edcc63f
Gilvan-R-A <gilvan.dev@gmail.com>
2025-07-19 11:36:33 -0300

    feat: create authentication configuration

--- news-frontend/src/app/services/usuario.service.ts
+++ news-frontend/src/app/services/usuario.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Usuario } from '../models/usuario';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
+  private apiUrl = 'http://localhost:3000/usuarios';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  login(email: string, senha: string): Observable<Usuario[]> {
+    return this.http.get<Usuario[]>(
+      `${this.apiUrl}?email=${email}&senha=${senha}`
+    );
+  }
+
+  cadastrar(usuario: Usuario): Observable<Usuario> {
+    return this.http.post<Usuario>(this.apiUrl, usuario);
+  }
+
+  salvarUsuarioLogado(usuario: Usuario): void {
+    localStorage.setItem('usuarioLogado', JSON.stringify(usuario));
+  }
+
+  obterUsuarioLogado(): Usuario | null {
+    const usuario = localStorage.getItem('usuarioLogado');
+    return usuario ? JSON.parse(usuario) : null;
+  }
+
+  logout(): void {
+    localStorage.removeItem('usuarioLogado');
+  }
+
+  estaAutenticado(): boolean {
+    return !!this.obterUsuarioLogado();
+  }
 }
