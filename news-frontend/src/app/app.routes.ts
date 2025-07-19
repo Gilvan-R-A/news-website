56ae50ae88e57fbae31614611c9ec4becb69c286
Gilvan-R-A <gilvan.dev@gmail.com>
2025-07-19 11:38:35 -0300

    feat: configure routes

--- news-frontend/src/app/app.routes.ts
+++ news-frontend/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { CadastroPostagemComponent } from './components/cadastro-postagem/cadastro-postagem.component';
+import { CadastroUsuarioComponent } from './components/cadastro-usuario/cadastro-usuario.component';
+import { AuthGuard } from './guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'cadastro-usuario', component: CadastroUsuarioComponent},
+    {
+        path: 'cadastro-postagem',
+        component: CadastroPostagemComponent,
+        canActivate: [AuthGuard]
+    }
+];
