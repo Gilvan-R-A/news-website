cf12e60067b8e673f6be1a08901404888c3cecd6
Gilvan-R-A <gilvan.dev@gmail.com>
2025-09-29 13:58:45 -0300

    feat: configure Postagem Service

--- news-frontend/src/app/services/postagem.service.ts
+++ news-frontend/src/app/services/postagem.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Postagem } from '../models/postagem';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostagemService {
+  private apiUrl = 'http://localhost:3000/postagens';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  listar(): Observable<Postagem[]> {
+    return this.http.get<Postagem[]>(this.apiUrl);
+  }
+
+  cadastrar(postagem: Postagem): Observable<Postagem> {
+    return this.http.post<Postagem>(this.apiUrl, postagem);
+  }
 }
