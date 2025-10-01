3693e2d1ed29a4582977350d1e75325ab7449441
Gilvan-R-A <gilvan.dev@gmail.com>
2025-10-01 12:52:55 -0300

    feat: adds post registration logic

--- news-frontend/src/app/components/cadastro-postagem/cadastro-postagem.component.ts
+++ news-frontend/src/app/components/cadastro-postagem/cadastro-postagem.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { PostagemService } from '../../services/postagem.service';
+import { Postagem } from '../../models/postagem';
 
 @Component({
   selector: 'app-cadastro-postagem',
@@ -7,5 +9,14 @@ import { Component } from '@angular/core';
   styleUrl: './cadastro-postagem.component.scss'
 })
 export class CadastroPostagemComponent {
+  postagem: Postagem = { titulo: '', conteudo: '', categoria: '', data: ''};
 
+  constructor(private postagemService: PostagemService) {}
+
+  cadastrar() {
+    this.postagemService.cadastrar(this.postagem).subscribe({
+      next: () => alert('Postegem cadastrada com sucesso!'),
+      error: (err) => console.error(err),
+    });
+  }
 }
