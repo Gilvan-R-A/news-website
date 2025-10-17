2c7d9fcf6d3c60b6f470cae41ad68be05809b18a
Gilvan-R-A <gilvan.dev@gmail.com>
2025-10-17 10:48:55 -0300

    feat: configures the post registration component

--- news-frontend/src/app/components/cadastro-postagem/cadastro-postagem.component.ts
+++ news-frontend/src/app/components/cadastro-postagem/cadastro-postagem.component.ts
@@ -1,21 +1,24 @@
 import { Component } from '@angular/core';
 import { PostagemService } from '../../services/postagem.service';
 import { Postagem } from '../../models/postagem';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cadastro-postagem',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './cadastro-postagem.component.html',
   styleUrl: './cadastro-postagem.component.scss'
 })
 export class CadastroPostagemComponent {
-  postagem: Postagem = { titulo: '', conteudo: '', categoria: '', data: ''};
+  postagem: Postagem = { titulo: '', conteudo: '', categoria: '', data: new Date().toISOString()};
 
   constructor(private postagemService: PostagemService) {}
 
   cadastrar() {
     this.postagemService.cadastrar(this.postagem).subscribe({
-      next: () => alert('Postegem cadastrada com sucesso!'),
+      next: () => alert('Postagem cadastrada com sucesso!'),
       error: (err) => console.error(err),
     });
   }
