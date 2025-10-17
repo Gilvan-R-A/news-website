bb346cb2f4681a6d79f5de1ecf18f9b256175302
Gilvan-R-A <gilvan.dev@gmail.com>
2025-10-17 11:16:02 -0300

    feat: configures the user registration component

--- news-frontend/src/app/components/cadastro-usuario/cadastro-usuario.component.ts
+++ news-frontend/src/app/components/cadastro-usuario/cadastro-usuario.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { Usuario } from '../../models/usuario';
+import { UsuarioService } from '../../services/usuario.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cadastro-usuario',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './cadastro-usuario.component.html',
   styleUrl: './cadastro-usuario.component.scss'
 })
 export class CadastroUsuarioComponent {
+  usuario: Usuario = { email: '', senha: ''};
 
+  constructor(private usuarioService: UsuarioService) {}
+
+  cadastrar() {
+    this.usuarioService.cadastrar(this.usuario).subscribe({
+      next: () => alert('Usuário cadastrado com sucesso!'),
+      error: (err) => console.error(err)
+    });
+  }
 }
--- news-frontend/src/app/models/usuario.ts
+++ news-frontend/src/app/models/usuario.ts
@@ -1,6 +1,5 @@
 export interface Usuario {
     id?: number;
-    nome: string;
     email: string;
     senha: string;
 }
