37bc5c598e1895eeb1d2deccb7927ce9c364050f
Gilvan-R-A <gilvan.dev@gmail.com>
2025-10-19 12:57:20 -0300

    feat: applies formatting to post dates

--- news-frontend/src/app/components/cadastro-postagem/cadastro-postagem.component.ts
+++ news-frontend/src/app/components/cadastro-postagem/cadastro-postagem.component.ts
@@ -2,23 +2,34 @@ import { Component } from '@angular/core';
 import { PostagemService } from '../../services/postagem.service';
 import { Postagem } from '../../models/postagem';
 import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-cadastro-postagem',
   standalone: true,
   imports: [CommonModule, FormsModule, NavbarComponent],
+  providers: [DatePipe],
   templateUrl: './cadastro-postagem.component.html',
   styleUrl: './cadastro-postagem.component.scss'
 })
 export class CadastroPostagemComponent {
   postagem: Postagem = { titulo: '', conteudo: '', categoria: '', data: new Date().toISOString()};
 
-  constructor(private postagemService: PostagemService) {}
+  constructor(
+    private postagemService: PostagemService, 
+    private datePipe: DatePipe
+  ) {}
 
   cadastrar() {
-    this.postagemService.cadastrar(this.postagem).subscribe({
+    const dataFormatada = this.datePipe.transform(new Date(), 'dd/MM/yyyy') || '';
+
+    const postagemComDataFormatada: Postagem = {
+      ...this.postagem,
+      data: dataFormatada
+    };
+
+    this.postagemService.cadastrar(postagemComDataFormatada).subscribe({
       next: () => {
         alert('Postagem cadastrada com sucesso!');
 
