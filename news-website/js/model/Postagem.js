165a82254ec1bbf711dead22c96141634fce007e
Gilvan-R-A <gilvan.dev@gmail.com>
2025-10-26 20:52:59 -0300

    remove: remove old project version and keep Angular version only

--- news-website/js/model/Postagem.js
+++
@@ -1,9 +0,0 @@
-export default class Postagem{
-    constructor(idPostagem, tituloPostagem, conteudoPostagem, categoriaPostagem){
-        this.idPostagem = idPostagem;
-        this.tituloPostagem = tituloPostagem;
-        this.conteudoPostagem = conteudoPostagem;
-        this.categoriaPostagem = categoriaPostagem;
-        this.dataPostagem = new Date().getDate() + '/' + (new Date().getMonth()+1) + '/' + new Date().getFullYear();
-    }
-}
--- news-website/js/model/Usuario.js
+++
@@ -1,7 +0,0 @@
-export default class Usuario{
-    constructor(idUsuario, emailUsuario, senhaUsuario){
-        this.idUsuario = idUsuario;
-        this.emailUsuario = emailUsuario;
-        this.senhaUsuario = senhaUsuario;
-    }
-}
